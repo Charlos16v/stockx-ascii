0be650b6a59d65007220ed4e4ec63dae6a9b03d6
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-03-03 11:36:46 +0100

    refactor: AndCriteria with stream

--- src/main/java/edu/pingpong/stockx/criteria/AndCriteria.java
+++ src/main/java/edu/pingpong/stockx/criteria/AndCriteria.java
@@ -5,6 +5,7 @@ import edu.pingpong.stockx.item.Item;
 import edu.pingpong.stockx.item.Offer;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class AndCriteria implements Criteria {
 
@@ -23,13 +24,12 @@ public class AndCriteria implements Criteria {
         List<Offer> criteria = this.criteria.checkCriteria(sneaker);
         List<Offer> otherCriteria = this.otherCriteria.checkCriteria(sneaker);
 
-        otherCriteria.retainAll(criteria);
-        return otherCriteria;
-
-        /*return criteria.stream()
+        /*criteria.retainAll(otherCriteria);
+        return criteria;*/
+        return criteria.stream()
                 .distinct()
                 .filter(otherCriteria::contains)
-                .collect(Collectors.toList());*/
+                .collect(Collectors.toList());
 
     }
 }
--- src/test/java/edu/pingpong/stockx/criteria/AndCriteriaTest.java
+++ src/test/java/edu/pingpong/stockx/criteria/AndCriteriaTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class AndCriteriaTest {
 
@@ -34,5 +35,37 @@ public class AndCriteriaTest {
 
         assertEquals(777, andCriteria.checkCriteria(sneaker).get(1).value());
         assertEquals("9.5", andCriteria.checkCriteria(sneaker).get(1).size());
+    }
+
+    @Test
+    public void andSizeBidCriteriaTest() {
+
+        Sneaker sneaker = new Sneaker("555088-105", "Jordan 1");
+        sneaker.add(new Bid("9.5", 550));
+        sneaker.add(new Bid("4.5", 480));
+        sneaker.add(new Bid("9.5", 900));
+        sneaker.add(new Bid("6", 472));
+        sneaker.add(new Ask("15", 288));
+        sneaker.add(new Ask("13", 333));
+        sneaker.add(new Ask("9.5", 1000));
+        sneaker.add(new Ask("13", 341));
+        assertEquals(8, sneaker.offers().size());
+
+        Criteria size = new Size("9.5");
+        Criteria bids = new Bids();
+
+        assertEquals(4, bids.checkCriteria(sneaker).size());
+        assertEquals(3, size.checkCriteria(sneaker).size());
+
+        Criteria andSizeBids = new AndCriteria(size, bids);
+        assertEquals(2, andSizeBids.checkCriteria(sneaker).size());
+
+        assertTrue(andSizeBids.checkCriteria(sneaker).stream().allMatch(o -> o.size().equals("9.5")));
+        assertTrue(andSizeBids.checkCriteria(sneaker).stream().allMatch(o -> o instanceof Bid));
+
+
+        // Changing filter position
+        Criteria andBidsSize = new AndCriteria(bids, size);
+        assertEquals(2, andBidsSize.checkCriteria(sneaker).size());
     }
 }
